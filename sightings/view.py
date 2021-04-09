da30f01af53cb7fd5489373b66d571734062a0ba
kemincao <kc3001@columbia.edu>
2021-04-09 14:00:06 +0000

    Deleted duplicate py file

--- sightings/view.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'map/index.html'), ())
