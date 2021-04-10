695c81d5600c850db9ed364fd297e941ae4aadb2
kemincao <kc3001@columbia.edu>
2021-04-10 09:37:52 +0000

    Update map views

--- map/views.py
+++ map/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from sightings.models import Squirrels
 
 def index(request):
-    return render(request, 'map/index.html', {})
+    squirrels = Squirrels.objects.all()[:100]
+    context = {'squirrels': squirrels}
+
+    return render(request, 'map/index.html', {context})
 # Create your views here.
