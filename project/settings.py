23f41e5bd10a59c31f36e440f75714b7bcb08c23
ql2418 <ql2418@columbia.edu>
2021-04-14 06:50:19 +0000

    fixed secret key to be able to run on local host

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY') 
+SECRET_KEY = 'django-insecure-kn3sh$cizl+mc+_9_2z%^(b3kd(9luak!b+m4302$o%jbtl&bm'
+##When deploying:
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
